5bd863e56a33c800f0b167c41c04b29b5da281f9
Josh Garvey <12745216+dataGuyJosh@users.noreply.github.com>
2024-02-08 19:50:23 +1100

    Validation/Gridsearch pseudo code

--- code/lda_pseudo.py
+++ code/lda_pseudo.py
@@ -17,6 +17,8 @@ def read_documents(file_path, num_docs=0, regex=''):
 
 
 '''# Preprocessing'''
+
+
 def word_processing(docs):
     new_docs = []
     '''
@@ -49,19 +51,6 @@ def n_grams(docs, n=3):
         pass
     return new_docs
 
-
-def tf_idf(docs, threshold):
-  new_docs = []
-  '''
-  Term Frequency - Inverse Document Frequency
-  - terms with high TF-IDF scores occur frequently within a document 
-    but relatively rarely in the rest of the corpus
-  - we assume that a high TF-IDF score indicates a given term is important to the context of the document
-  - set a threshold score at which we accept/deny terms for being used
-  - filter documents to only include terms which pass this threshold
-  '''
-  return new_docs
-
 def get_bow(docs):
     '''
     https://radimrehurek.com/gensim/models/ldamodel.html
@@ -69,10 +58,27 @@ def get_bow(docs):
     - corpus: a 2D list of tuples where the first value indicates a term (as an index)
       and the second value indicates the frequency of that term in the document
     - id2word: a list of terms mapping to the indexes in corpus
+    Both of these can be generated using gensim
+    
     '''
     return [id2word, corpus]
 
-def train_lda_model(id2word, corpus):
+
+def tf_idf(id2word, corpus, threshold=0.03):
+    new_docs = []
+    '''
+  Term Frequency - Inverse Document Frequency
+  - terms with high TF-IDF scores occur frequently within a document 
+    but relatively rarely in the rest of the corpus
+  - we assume that a high TF-IDF score indicates a given term is important to the context of the document
+  - set a threshold score at which we accept/deny terms for being used
+  - filter documents to only include terms which pass this threshold
+  '''
+    return new_docs
+
+
+'''# Model Training/Validation'''
+def train_lda_model(id2word, corpus, hyper_parameters=[10, 1, 100]):
     '''
     At this point we have our data-based inputs, define hyper parameters and run the model.
 
@@ -84,6 +90,34 @@ def train_lda_model(id2word, corpus):
     '''
     pass
 
+
+def validate_model(model, id2word, corpus, metric):
+    '''
+    https://github.com/MIND-Lab/OCTIS
+    Using the octis library we can perform model comparisons using various metrics, including Coherence.
+    For a given model/metric, return result e.g. average coherence on all topics
+    '''
+    pass
+
+
+def grid_search(id2word, corpus):
+    '''
+    Iterate through all combinations of a set of hyper-parameters,
+    evaluating each setup's performance each time to find the best combination
+    Example params
+    'num_topics': [5, 10, 15, 20]
+    'update_every': [1, 2, 3]
+    'chunksize': [100, 110, 120]
+
+    for each combinations of hyper parameters
+      train model --> train_lda_model
+      calculate topic coherences --> validate_model
+      summarize model based on coherences e.g. average topic coherence
+
+    return model with highest average coherence
+    '''
+
+
 def visualize_lda_model(model, id2word, corpus):
     '''
     This LDA visualization tool works with gensim models (and was shamelessly stolen from a tutorial, see resources in README.md)
@@ -93,14 +127,18 @@ def visualize_lda_model(model, id2word, corpus):
     '''
     pass
 
+
 if __name__ == "__main__":
     # read documents
     docs = read_documents('data/20newsgroups_9000.txt')
     # perform term preprocessing
-    docs_words = tf_idf(n_grams(word_processing(docs)), 0.03)
-    # generate model inputs
+    docs_words = n_grams(word_processing(docs))
+    # generate dictionary/word index (bag-of-words?)
     id2word, corpus = get_bow(docs_words)
+    # filter words based on tf-idf
+    corpus = tf_idf(id2word, corpus)
     # train model
     model = train_lda_model(id2word, corpus)
+    # model = grid_search(id2word, corpus)
     # visualize
     visualize_lda_model(model, id2word, corpus)
